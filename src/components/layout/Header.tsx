d50a3e8aa9fcb722798bde06cb0c918bdc67b839
cedJey <cedrickandriamihaja@gmail.com>
2025-05-23 14:13:49 +0300

    updated

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -4,20 +4,19 @@ import Image from 'next/image';
 
 export default function Header() {
   return (
-    <header id="header" className="header flex items-center light-background sticky top-0">
-      <div className="container-fluid relative flex items-center justify-between w-full px-4 py-2">
-        <Link href="/" className="logo flex items-center me-auto me-xl-0">
-          <Image src="/logo.png" alt="Logo" width={40} height={40} />
-          <h1 className="">Cedrick</h1>
-        </Link>
+    <header className="header flex items-center light-background sticky top-0">
+      <div className="container mx-auto relative flex items-center justify-between w-full px-4 py-2">
+        <div className="">
+          <Link href="/" className="flex items-center me-auto me-xl-0">
+            <Image className='rounded-full' src="/images/logo.png" width={60} height={100} alt="Logo" />
+          </Link>
+        </div>
 
-        <nav id="navmenu" className="navmenu">
+        <nav  className="navmenu">
           <ul className="flex space-x-6">
             <li><Link href="/" className="active text-blue-500">Home</Link></li>
-            <li><Link href="/about" className="hover:text-blue-500">About</Link></li>
-            <li><Link href="/resume" className="hover:text-blue-500">Resume</Link></li>
-            <li><Link href="/services" className="hover:text-blue-500">Services</Link></li>
             <li><Link href="/portfolio" className="hover:text-blue-500">Portfolio</Link></li>
+            <li><Link href="/about" className="hover:text-blue-500">About</Link></li>
             <li><Link href="/contact" className="hover:text-blue-500">Contact</Link></li>
           </ul>
           <i className="mobile-nav-toggle xl:hidden bi bi-list"></i>
